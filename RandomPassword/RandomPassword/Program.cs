e7b5fd38dff8188633a1e789a158061dbe44bf5d
Kabmeister <51206573+Kabmeister@users.noreply.github.com>
2019-05-31 10:23:17 +0100

    First Github

---
+++ RandomPassword/RandomPassword/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RandomPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PasswordLength = 10;
+            var random = new Random();
+            var buffer = new char[PasswordLength];
+
+            for (int i = 0; i < PasswordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
